2cd341db371ec6c27cce97ce96d6892fc62df015
Edvaldo Gesteira <leongesteira@gmail.com>
2026-04-30 17:14:46 -0300

    Inclusão do código HTML, JS e CSS

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function somar() {
+    let n1 = document.getElementById("num1").value;
+    let n2 = document.getElementById("num2").value;
+
+    // Aqui até poderíamos somar, mas não é necessário
+    // let soma = Number(n1) + Number(n2);
+
+    document.getElementById("resultado").value = "Vai Te Tomar No Meio do Centro do Olho do seu CÚ!!!";
+}
