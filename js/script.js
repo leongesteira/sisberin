671acf68d3cb6c6173d1a208b7ada3435f079552
Edvaldo Gesteira <leongesteira@gmail.com>
2026-04-30 17:37:40 -0300

    Atualização do código e inserção da imagem

--- js/script.js
+++ js/script.js
@@ -6,4 +6,7 @@ function somar() {
     // let soma = Number(n1) + Number(n2);
 
     document.getElementById("resultado").value = "Vai Te Tomar No Meio do Centro do Olho do seu CÚ!!!";
+
+    // Mostrar a imagem
+    document.getElementById("imagemAmor").style.display = "block";
 }
