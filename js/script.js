17382dc43f41592c9ce77e68b358753f3ed1d2f1
Edvaldo Gesteira <leongesteira@gmail.com>
2026-05-04 14:37:51 -0300

    Atualização do resultado da calculadora

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function somar(){
     let n1 = Number(document.getElementById("num1").value);
     let n2 = Number(document.getElementById("num2").value);
     
-    document.getElementById("resultado").value = "Vai Te Tomar No Centro Do Olho Do Meio Do Seu CÙ!!";
+    document.getElementById("resultado").value = "Vai Te Tomar Bem no Meio Do Centro Do Olho Do Seu CÚ!!!";
 
     document.getElementById("imagemAmor").style.display = "block"
 
